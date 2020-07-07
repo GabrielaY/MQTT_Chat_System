3e61c43d883f4b3994d867f1178a595d13246653
Ognian Baruh <ognian@baruh.net>
2020-07-07 14:20:20 +0300

    [New] Added request/response pattern for MQTT 3

--- Python-Application/MQTTAdmin.py
+++ Python-Application/MQTTAdmin.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import paho.mqtt.client as mqtt
 
@@ -8,9 +9,26 @@ def on_connect(self, userdata, flags, rc):
 
 
 # The callback for when a PUBLISH message is received from the server.
-def on_message(self, userdata, message):
-    message = json.loads(str(message.payload.decode("utf-8")))
-    print(message["sender"] + ": " + message["content"] + " " + message["timestamp"])
+def on_message(self, userdata, msg):
+    received_message = json.loads(str(msg.payload.decode("utf-8")))
+
+    if msg.topic == "/chat/system/request":
+        if received_message["sender"][:1] == "a":
+            response = json.dumps({
+                "content": "yes",
+                "timestamp": datetime.now().strftime("%T")
+            })
+        else:
+            response = json.dumps({
+                "content": "no",
+                "timestamp": datetime.now().strftime("%T")
+            })
+
+        response_topic = "/chat/" + received_message["sender"] + "/response"
+        admin.publish(response_topic, response)
+
+    else:
+        print(received_message["sender"] + ": " + received_message["content"] + " " + received_message["timestamp"])
 
 
 admin = mqtt.Client("Admin", True, None, mqtt.MQTTv31)
@@ -19,4 +37,10 @@ admin.on_message = on_message
 
 admin.connect("mqtt.eclipse.org", 1883, 60)
 admin.subscribe("/chat/system")
-admin.loop_forever()
+admin.subscribe("/chat/system/request")
+admin.loop_start()
+
+# Option for admin to terminate (on text input)
+text = input()
+admin.disconnect()
+admin.loop_stop()
