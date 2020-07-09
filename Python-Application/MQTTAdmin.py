d3c2ce9644ad424330320001f3b2aee1fda4d815
GabrielaY <gabrielay2002@gmail.com>
2020-07-09 10:56:45 +0300

    [Update] Integrated protocol buffer with python application

--- Python-Application/MQTTAdmin.py
+++ Python-Application/MQTTAdmin.py
@@ -1,28 +1,41 @@
 from datetime import datetime
 import json
 import paho.mqtt.client as mqtt
+import ProtoBuffer.ChatPrototypes_pb2 as ChatPrototypes
+from google.protobuf.json_format import MessageToDict, MessageToJson
+
+# from MQTTClient import topics, system_messages, create_message
+
+topics = ChatPrototypes.Topics()
+system_messages = ChatPrototypes.SystemMessages()
+
+
+# Helper function for creating message
+def create_message(content, sender_name):
+    chat_message = ChatPrototypes.ChatMessage()
+    chat_message.content = content
+    chat_message.timestamp = datetime.now().strftime("%T")
+    return MessageToJson(chat_message)
 
 
 # The callback for when the admin receives a CONNACK response from the server.
 def on_connect(self, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    print("connected")
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(self, userdata, msg):
+
     received_message = json.loads(str(msg.payload.decode("utf-8")))
 
-    if msg.topic == "/chat/system/request":
+    if msg.topic == topics.system_request_topic:
+        print("in")
         if received_message["sender"][:1] == "a":
-            response = json.dumps({
-                "content": "yes",
-                "timestamp": datetime.now().strftime("%T")
-            })
+            response = create_message(system_messages.approve_join_text, None)
+
         else:
-            response = json.dumps({
-                "content": "no",
-                "timestamp": datetime.now().strftime("%T")
-            })
+            response = create_message(system_messages.deny_join_text, None)
+            print("not approved")
 
         response_topic = "/chat/" + received_message["sender"] + "/response"
         admin.publish(response_topic, response)
@@ -36,8 +49,8 @@ admin.on_connect = on_connect
 admin.on_message = on_message
 
 admin.connect("mqtt.eclipse.org", 1883, 60)
-admin.subscribe("/chat/system")
-admin.subscribe("/chat/system/request")
+admin.subscribe(topics.system_topic)
+admin.subscribe(topics.system_request_topic)
 admin.loop_start()
 
 # Option for admin to terminate (on text input)
