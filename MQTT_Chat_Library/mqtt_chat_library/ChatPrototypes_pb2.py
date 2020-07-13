8d0b621a753d2465849f9fd29469f99d80c62239
Ognian Baruh <ognian@baruh.net>
2020-07-13 14:25:32 +0300

    [Update] Version 1.0.2 of library -> update .proto file

--- MQTT_Chat_Library/mqtt_chat_library/ChatPrototypes_pb2.py
+++ MQTT_Chat_Library/mqtt_chat_library/ChatPrototypes_pb2.py
@@ -10,6 +10,9 @@ from google.protobuf import symbol_database as _symbol_database
 
 _sym_db = _symbol_database.Default()
 
+
+
+
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='ChatPrototypes.proto',
   package='message',
