efd697baca08a0f3659536d051c75acf7d0d3b7d
Ognian Baruh <ognian@baruh.net>
2020-07-14 10:54:50 +0300

    [Update] Version 1.1.0 of library -> Bug fixes, client_id fixes

--- MQTT_Chat_Library/mqtt_chat_library/mqtt_chat_library.py
+++ MQTT_Chat_Library/mqtt_chat_library/mqtt_chat_library.py
@@ -26,23 +26,26 @@ def create_message(content, sender_name):
 # The callback for when the client receives a CONNACK response from the server.
 def client_on_connect(client, userdata, flags, rc):
     print("Connected")
-    system_message = create_message(system_messages.connected_text, client._client_id)
+    system_message = create_message(system_messages.connected_text, client._client_id.decode("utf-8"))
     client.publish(topics.system_topic, system_message)
 
 
 # The callback for when a PUBLISH message is received from the server.
-def client_on_message(self, client, userdata, msg):
+def client_on_message(client, userdata, msg):
     received_message = json.loads(str(msg.payload.decode("utf-8")))
-
-    if msg.topic == "/chat/" + client._client_id + "/response":  # /chat/response/ + client_name
-        
+    client_id = client._client_id.decode("utf-8")
+    
+    if msg.topic == "/chat/" + client_id + "/response":  # /chat/response/ + client_name
+        global response_for_join_request
         if received_message["content"] == system_messages.approve_join_text:
             print(system_messages.joined_text)
             client.subscribe(topics.chat_topic)
+            response_for_join_request = 1
         else:
             print(system_messages.access_denied_text)
-            disconnect_message = create_message(system_messages.disconnected_text, client._client_id)
+            disconnect_message = create_message(system_messages.disconnected_text, client_id)
             client.publish(topics.system_topic, disconnect_message)
+            response_for_join_request = -1
 
     else:
         print(received_message["sender"] + ": " + received_message["content"] + " " + received_message["timestamp"])
@@ -54,17 +57,15 @@ def admin_on_connect(self, userdata, flags, rc):
 
 
 # The callback for when a PUBLISH message is received from the server.
-def admin_on_message(self, client, userdata, msg):
+def admin_on_message(client, userdata, msg):
     received_message = json.loads(str(msg.payload.decode("utf-8")))
+    client_id = client._client_id.decode("utf-8")
 
     if msg.topic == topics.system_request_topic:
-        global response_for_join_request
         if received_message["sender"][:1] == "a":
-            response = create_message(system_messages.approve_join_text, None)
-            response_for_join_request = 1
+            response = create_message(system_messages.approve_join_text, client_id)
         else:
-            response = create_message(system_messages.deny_join_text, None)
-            response_for_join_request = -1
+            response = create_message(system_messages.deny_join_text, client_id)
 
         response_topic = "/chat/" + received_message["sender"] + "/response"
         client.publish(response_topic, response)
