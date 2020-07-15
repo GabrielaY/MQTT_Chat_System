563b2937f8d03e49e4ebb5db42756d524e8624d1
GabrielaY <gabrielay2002@gmail.com>
2020-07-15 15:26:05 +0300

    [Update] Logging extended

--- Python-Application/MQTTClient.py
+++ Python-Application/MQTTClient.py
@@ -1,27 +1,42 @@
 import paho.mqtt.client as mqtt
+from logging.handlers import RotatingFileHandler
 import mqtt_chat_library.mqtt_chat_library as MqttChatLibrary
 from log_config import info_log, error_log
+
 
 topics = MqttChatLibrary.topics
 system_messages = MqttChatLibrary.system_messages
 
+# Choose logging method
+path_for_logging = input("Enter logging path: ")
+if path_for_logging != "default" and path_for_logging!="console":
+    info_handling = RotatingFileHandler(path_for_logging, maxBytes=1000, backupCount=1)
+    error_handling = RotatingFileHandler(path_for_logging, maxBytes=1000, backupCount=1)
+    info_log.addHandler(info_handling)
+    error_log.addHandler(error_handling)
+
 # Create client
 client_name = input("Enter username: ")
 python_client = mqtt.Client(client_name, True, None, mqtt.MQTTv31)
+if path_for_logging == "console":
+    print("Python client with ID: %s has been initialized" % client_name)
+else:
+    info_log.info("Python client with ID: %s has been initialized" % client_name)
 python_client.on_connect = MqttChatLibrary.client_on_connect
 python_client.on_message = MqttChatLibrary.client_on_message
 
 # Connect client
 python_client.connect("mqtt.eclipse.org", 1883, 60)
-info_log.info("%s connected to broker" % client_name)
+if path_for_logging == "console":
+    print("%s connected to broker" % client_name)
+else:
+    info_log.info("%s connected to broker" % client_name)
 python_client.subscribe(topics.system_response_topic + client_name)
-info_log.info("%s subscribed to topic: %s" % (client_name, topics.system_request_topic + client_name))
 python_client.loop_start()
 
 # Request group access
 message = MqttChatLibrary.create_message(system_messages.request_join_text, client_name)
 python_client.publish(topics.system_request_topic, message)
-info_log.info("%s published a message to topic: %s" % (client_name, topics.system_request_topic))
 
 # Wait for response
 response = MqttChatLibrary.wait_for_response_for_join_request()
@@ -37,12 +52,14 @@ while True:
     if text == "QUIT":
         message = MqttChatLibrary.create_message(system_messages.disconnected_text, client_name)
         python_client.publish(topics.system_topic, message)
-        info_log.info("%s disconnected from broker" % client_name)
+        if path_for_logging == "console":
+            print("%s disconnected from broker" % client_name)
+        else:
+            info_log.info("%s disconnected from broker" % client_name)
         break
 
     message = MqttChatLibrary.create_message(text, client_name)
     python_client.publish(topics.chat_topic, message)
-    info_log.info("%s published a message to topic: %s" % (client_name, topics.chat_topic))
 
 # Disconnect client
 python_client.disconnect()
